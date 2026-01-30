c6273288fb7b84673b248698844e2bfbc9c833e0
gueon <kge9605@naver.com>
2026-01-30 15:34:25 +0900

    Feat: Add 자원을 지게처럼 등에 쌓아 올리게 List로 구현

---
+++ Assets/02_Scripts/Mineral/StackBackPack.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StackBackPack : MonoBehaviour
+{
+    [SerializeField] private List<Transform> acquiredResources = new List<Transform>(); //가방에 담길 리스트
+    [SerializeField] GameObject player;   //플레이어
+    [SerializeField] Transform backPackPos; //가방 위치
+    public float itemHeight = 0.3f; //아이템 높이 간격
+
+    [SerializeField] private int maxCapacity = 10; //최대 수용량
+
+    private void Start()
+    {
+        if (backPackPos == null)
+        {
+            backPackPos = GameObject.Find("BackPackPos").transform;
+        }
+    }
+    private void Update()
+    {
+        if (acquiredResources.Count == 0) return;
+
+        //가방 시작 위치
+        if (acquiredResources.Count > 0)
+        {
+            acquiredResources[0].position = Vector3.Lerp(acquiredResources[0].position, backPackPos.position, Time.deltaTime * 10f);
+            acquiredResources[0].rotation = Quaternion.Lerp(acquiredResources[0].rotation, backPackPos.rotation, Time.deltaTime * 10f);
+        }
+
+        for(int i = 1; i < acquiredResources.Count; i++)
+        {
+            Transform currentAcquired = acquiredResources[i];
+            Transform previousAcquired = acquiredResources[i - 1];
+
+            Vector3 targetPos = previousAcquired.position + Vector3.up * itemHeight;
+
+            currentAcquired.position = Vector3.Lerp(currentAcquired.position, targetPos, Time.deltaTime * 10f);
+            currentAcquired.rotation = Quaternion.Lerp(currentAcquired.rotation, backPackPos.rotation, Time.deltaTime * 10f);
+        }
+    }
+    public void AddResources(GameObject resources)
+    {
+        if(resources == null) return;
+
+        acquiredResources.Add(resources.transform);
+
+        resources.transform.SetParent(backPackPos, true);
+
+        Collider col = resources.GetComponent<Collider>();
+        if(col) col.enabled = false;
+
+        Destroy(resources.GetComponent<Rigidbody>());
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Resources")) //TODO: 자원에서 창고로 쓰이는 테이블 Tag, Layer 로 체인지
+        {
+            if (acquiredResources.Count >= maxCapacity) return; //가방이 지정한 갯수만큼 꽉 찼으면 리턴
+            AddResources(other.gameObject);
+        }
+    }
+}
---
+++ Assets/02_Scripts/Mineral/TestPlayerMove.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayerMove : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    Rigidbody rb;
+    public GameObject player;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();        
+    }
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 dir = (Vector3.forward * v) + (Vector3.right * h);
+
+        player.transform.position += dir * moveSpeed * Time.deltaTime;
+    }
+}
