4f212c0c49a1a65b063838e0674aa7be6b155ebd
gueon <kge9605@naver.com>
2026-02-12 01:05:44 +0900

    fix: PriorityQueue 주석처리

--- Assets/02_Scripts/AutoSystem/AStar/PriorityQueue.cs
+++ Assets/02_Scripts/AutoSystem/AStar/PriorityQueue.cs
@@ -1,7 +1,11 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
+/// <summary>
+/// 우선순위 큐 구현 클래스
+/// Enqueue: 아이템과 우선순위를 함께 추가
+/// Dequeue: 가장 높은 우선순위(가장 낮은 숫자)의 아이템을 탐색하고 제거(꺼냄)
+/// </summary>
+/// <typeparam name="T"></typeparam>
 public class PriorityQueue<T>
 {
     private List<(T item, int priority)> elements = new List<(T item, int priority)>();
@@ -15,6 +19,8 @@ public class PriorityQueue<T>
     public T Dequeue()
     {
         int bestIndex = 0;
+
+        //가장 높은 우선순위(가장 낮은 숫자)의 아이템 탐색
         for (int i = 1; i < elements.Count; i++)
         {
             if (elements[i].priority < elements[bestIndex].priority)
@@ -22,10 +28,9 @@ public class PriorityQueue<T>
                 bestIndex = i;
             }
         }
+
         T bestItem = elements[bestIndex].item;
-
         elements.RemoveAt(bestIndex);
-
         return bestItem;
     }
 }
